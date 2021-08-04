84190ccaa5aa63b8325510091c052c6de43d75d6
RoMeAh <runa.ronit3129@gmail.com>
2021-08-04 14:18:54 +0530

    ✨ feat: made use of inquirer lib

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 #!/usr/bin/env node
-
 import { exec } from "child_process";
+import { prompt } from "inquirer";
 import { exit } from "process";
-import { input } from "./input";
 
 interface Types {
   types: { [key: string]: Type };
@@ -16,33 +15,37 @@ interface Type {
 const { types }: Types = require("../types.json");
 const keys = Object.keys(types);
 
-for (const type in types) {
-  const { title, description } = types[type];
-  console.log(
-    `${keys.indexOf(type) + 1}. ${type}: ${title} -> ${description}\n`
-  );
-}
-
 (async () => {
-  const type = parseInt(await input("Which commit type to use?: "));
-  if (type < 0 || type > keys.length - 1) {
-    console.error("Invalid commit type!");
-    exit(1);
-  }
+  const { type, scope, msg } = await prompt([
+    {
+      message: "Commit Type",
+      name: "type",
+      type: "list",
+      choices: keys.map((x) => ({
+        name: `${x}: ${types[x].title} -> ${types[x].title}`,
+        value: x,
+      })),
+    },
+    {
+      message: "Any specific file(s)?. If no, press enter.",
+      name: "scope",
+      type: "input",
+    },
+    {
+      message: "Commit Message",
+      name: "msg",
+      type: "input",
+    },
+  ]);
 
-  const commit = keys[type - 1];
-  const message = commit + (await input(`${commit}`));
-  if (!message.includes(":")) {
-    console.error("Error: Commit Message must contain ':'");
-    exit(1);
-  }
+  const message = `${type}${scope.length > 0 ? `(${scope})` : ""}: ${msg}`;
 
   exec(`git add .; git commit -m "${message}"`, (error, stdout, stderr) => {
     if (error) {
       console.error(`Git error: ${error}`);
       exit(1);
     }
-    console.log(stdout);
-    console.error(stderr);
+    if (stdout.length > 0) console.log(stdout);
+    if (stderr.length > 0) console.error(stderr);
   });
 })();
